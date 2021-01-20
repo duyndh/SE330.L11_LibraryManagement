fa5d9fb630dc007ac66f567e0644f2f2084f00d8
duyndh <hoangduypok98@gmail.com>
2021-01-20 20:23:20 +0700

    Add login panel

---
+++ Client/src/com/java/panels/LoginPanel.java
@@ -0,0 +1,55 @@
+package com.java.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginPanel {
+
+    private JPanel _panel;
+
+    public LoginPanel() {
+
+        _panel = new JPanel(new GridBagLayout());
+        //pane.
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.weightx = 10;
+        constraints.weighty = 10;
+        constraints.insets = new Insets( 2, 10, 2, 10 );
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        var usernameLabel = new JLabel("Username");
+        constraints.gridwidth = 1;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        _panel.add(usernameLabel, constraints);
+
+        var usernameTextField = new JTextField();
+        constraints.gridwidth = 1;
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        _panel.add(usernameTextField, constraints);
+
+        var passwordLabel = new JLabel("Password");
+        constraints.gridwidth = 1;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        _panel.add(passwordLabel, constraints);
+
+        var passwordTextField = new JTextField();
+        constraints.gridwidth = 1;
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        _panel.add(passwordTextField, constraints);
+
+        var loginButton = new JButton("Login");
+        constraints.gridwidth = 2;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        _panel.add(loginButton, constraints);
+    }
+
+    public JPanel get_panel() {
+        return _panel;
+    }
+}
--- Client/src/com/java/project/Main.java
+++ Client/src/com/java/project/Main.java
@@ -1,8 +1,10 @@
 package com.java.project;
 
 import com.java.panels.Home;
+import com.java.panels.LoginPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
@@ -11,13 +13,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var frame = new JFrame("Library Management");
-        frame.setContentPane(_home.get_homePanel());
-        frame.pack();
+        //var loginPanel = new Login();
+        //loginPanel
+
+        JFrame frame = new JFrame("Login");
+        frame.setContentPane(new LoginPanel().get_panel());
         frame.setVisible(true);
-        frame.setSize(1366, 768);
-        //frame.setSize(600, 400);
+        frame.setSize(300, 200);
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        return;
+
+//        var p = new GridBagLayout();
+//
+//        var frame = new JFrame("Library Management");
+//        frame.setContentPane(_home.get_homePanel());
+//        //frame.setContentPane(loginPanel.get_loginPanel());
+//        frame.pack();
+//        frame.setVisible(true);
+//        //frame.setSize(1366, 768);
+//        frame.setSize(600, 400);
+//        frame.setResizable(false);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
